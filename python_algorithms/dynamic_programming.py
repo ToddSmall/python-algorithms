95f630cc2377f008a205271ad71d4f05fe00af75
Todd Small <todd.andrew.small@gmail.com>
2018-06-15 22:48:15 -0700

    WIP

--- python_algorithms/dynamic_programming.py
+++ python_algorithms/dynamic_programming.py
@@ -2,10 +2,10 @@
 
 from functools import wraps
 import sys
-from typing import List
+from typing import Callable, List, Tuple
 
 
-def memoized(f):
+def memoized(f: Callable) -> Callable:
     """Decorator that memoizes the wrapped function."""
     cache = {}
     @wraps(f)
@@ -33,25 +33,26 @@ def cut_rod(prices: List[float], rod_length: int) -> float:
     return _cut_rod(rod_length)
 
 
-def knapsack(items, max_weight):
+def knapsack(items: List[Tuple[int, int]], max_weight: int) -> Tuple[int, List[Tuple[int, int]]]:
     """The classic 0/1 knapsack problem.
 
     https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem
     http://codereview.stackexchange.com/a/20581
     """
     @memoized
-    def best_value(i, w):
+    def best_value(i: int, w: int) -> int:
         """Return the best value that can be attained using the
         first i items with weight <= w.
         """
         if i == 0:
             return 0
+
         value, weight = items[i - 1]
+
         if weight > w:
             return best_value(i - 1, w)
 
-        return max(best_value(i - 1, w),
-                   best_value(i - 1, w - weight) + value)
+        return max(best_value(i - 1, w), best_value(i - 1, w - weight) + value)
 
     # This code block is only necessary for listing the items.
     # The best value can be, and is, computed without this
