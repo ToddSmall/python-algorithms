496591b6c8534ce901ead66adc895c7cfadae130
Todd Small <todd.andrew.small@gmail.com>
2018-06-20 21:06:15 -0700

    Typing hinting fixes

--- python_algorithms/misc.py
+++ python_algorithms/misc.py
@@ -1,14 +1,18 @@
 """An assortment of solutions to easy CS problems."""
 
-from typing import List, Tuple
+from typing import List, Generator, Tuple
 
 
-def gen_product_pairs(xs: List[float], prod: float) -> List[Tuple[float, float]]:
-    """Search the list `xs` for pairs whose product equals `prod`."""
-    factors = set()
+def gen_product_pairs(
+    xs: List[float], prod: float
+) -> Generator[Tuple[float, float], None, None]:
+    """Search the list `xs` for pairs whose product equals `prod`.
+    
+    https://codereview.stackexchange.com/a/84721
+    """
+    factors: set = set()
     for x in xs:
         if x in factors:
             yield prod / x, x
         else:
             factors.add(prod / x)
-        
